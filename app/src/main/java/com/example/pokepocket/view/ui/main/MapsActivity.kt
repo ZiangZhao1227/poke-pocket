5233efb81e0c62c48d3f3cac46e5692ea0d88352
Maksim Pasnitsenko <emexpas@outlook.com>
2021-09-27 19:43:21 +0300

    map test, pokemon png's

--- app/src/main/java/com/example/pokepocket/view/ui/main/MapsActivity.kt
+++ app/src/main/java/com/example/pokepocket/view/ui/main/MapsActivity.kt
@@ -41,9 +41,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in Leppävaara and move the camera
+        val leppavaara = LatLng(60.212728, 24.812588)
+        mMap.addMarker(MarkerOptions().position(leppavaara).title("Marker in Leppävaara"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(leppavaara))
     }
 }
