85618da8c61a403d342680bc1a104868604007a8
ziangz <zhaoziang1227@gmail.com>
2021-09-27 18:22:36 +0300

    fixed search bar bug by using life cycle onResume

--- app/src/main/java/com/example/pokepocket/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pokepocket/view/ui/main/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.pokepocket.view.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pokepocket.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (searchView != null) {
+            searchView.clearFocus();
+        }
     }
 }
