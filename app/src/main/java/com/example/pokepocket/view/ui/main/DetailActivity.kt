358574857a8372aae663142e01b630eb36a440bf
ziangz <zhaoziang1227@gmail.com>
2021-09-27 18:09:50 +0300

    detail page ui done

---
+++ app/src/main/java/com/example/pokepocket/view/ui/main/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokepocket.view.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pokepocket.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
