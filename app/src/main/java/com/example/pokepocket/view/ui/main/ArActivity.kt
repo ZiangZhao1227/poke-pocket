a4eec45a24cde3e59bf5bb00868a7ed54263280c
Maksim Pasnitsenko <emexpas@outlook.com>
2021-10-13 13:57:45 +0300

    test model

--- app/src/main/java/com/example/pokepocket/view/ui/main/ArActivity.kt
+++ app/src/main/java/com/example/pokepocket/view/ui/main/ArActivity.kt
@@ -53,7 +53,7 @@ class ArActivity : AppCompatActivity() {
         ModelRenderable.builder()
             .setSource(
                 this,
-                Uri.parse("https://github.com/ZiangZhao1227/poke_pocket/tree/ar/app/src/main/java/com/example/pokepocket/3d/pokeball.gltf")
+                Uri.parse("https://github.com/ZiangZhao1227/poke_pocket/tree/ar/app/src/main/java/com/example/pokepocket/3d/red_pokeball.gltf")
             )
             .setIsFilamentGltf(true)
             .setAsyncLoadEnabled(true)
